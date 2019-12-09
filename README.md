bbaa471efdef8307a9d8595fd90c7360949ef6e6
jacobhsu <jacob.hsu.tw@gmail.com>
2019-12-09 15:06:22 +0800

    gulp.task gulp.series gulp.parallel copyDevTaskNames

--- README.md
+++ README.md
@@ -21,3 +21,38 @@ gulp.task('dev:reload', function () {
 ```
 
 [gulp-livereload](http://kejyun.github.io/gulp-learning-notes/plguins/Tool/Plugins-Tool-gulp-livereload.html)  
+
+$ npm install --save-dev gulp-watch gulp-livereload
+
+## Usage
+
+```js
+  "scripts": {
+    "start": "gulp dev:extension",
+```
+
+`$ yarn start`
+
+Finished 'dev:copy:locales' after 84 ms
+Finished 'dev:copy' after 91 ms
+
+## Notes
+
+copyDevTaskNames 放入如語系打包工作
+gulp任務系列並行執行輸出到dist
+
+```js
+gulp.task('dev:copy',
+  gulp.series(
+    gulp.parallel(...copyDevTaskNames),
+  )
+)
+```
+
+`$ gulp`
+
+Task never defined: default
+To list available tasks, try running: `gulp --tasks`
+
+使用 `gulp.series()` 來打包任務
+`gulp.parallel()` 能夠在非同步狀態下同時進行此任務
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,92 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const livereload = require('gulp-livereload')
+
+const browserPlatforms = [
+    'firefox',
+    'chrome',
+]
+const commonPlatforms = [
+    // browser extensions
+    ...browserPlatforms,
+]
+ 
+
+// browser reload
+
+gulp.task('dev:reload', function () {
+    livereload.listen({
+      port: 35729,
+    })
+})
+
+// copy universal
+
+  const copyTaskNames = []
+  const copyDevTaskNames = []
+
+  createCopyTasks('locales', {
+    source: './app/_locales/',
+    destinations: commonPlatforms.map(platform => `./dist/${platform}/_locales`),
+  })
+
+  function createCopyTasks (label, opts) {
+    if (!opts.devOnly) {
+      const copyTaskName = `copy:${label}`
+      copyTask(copyTaskName, opts)
+      copyTaskNames.push(copyTaskName)
+    }
+    const copyDevTaskName = `dev:copy:${label}`
+    copyTask(copyDevTaskName, Object.assign({ devMode: true }, opts))
+    copyDevTaskNames.push(copyDevTaskName)
   }
+
+  function copyTask (taskName, opts) {
+    const source = opts.source
+    const destination = opts.destination
+    const destinations = opts.destinations || [destination]
+    const pattern = opts.pattern || '/**/*'
+    const devMode = opts.devMode
   
-  exports.default = defaultTask
+    return gulp.task(taskName, function () {
+      if (devMode) {
+        watch(source + pattern, (event) => {
+          livereload.changed(event.path)
+          performCopy()
+        })
+      }
+  
+      return performCopy()
+    })
+  
+    function performCopy () {
+      // stream from source
+      let stream = gulp.src(source + pattern, { base: source })
+  
+      // copy to destinations
+      destinations.forEach(function (destination) {
+        stream = stream.pipe(gulp.dest(destination))
+      })
+  
+      return stream
+    }
+  }
+
+// manifest tinkering
+gulp.task('dev:copy',
+  gulp.series(
+    gulp.parallel(...copyDevTaskNames),
+  )
+)
+
+
+// high level tasks
+
+gulp.task('dev:extension',
+  gulp.series(
+    gulp.parallel(
+      'dev:copy',
+      'dev:reload'
+    )
+  )
+)
